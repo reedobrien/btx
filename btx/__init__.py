7939c9794c2db678e1c4d1044a79111e146a940f
Reed O'Brien <reed@reedobrien.com>
2014-12-03 16:40:08 -0500

    Dorking around... use boto3 instead

--- btx/__init__.py
+++ btx/__init__.py
@@ -1,4 +1,4 @@
-"""
+"""=
 Copyright 2013 Reed O'Brien <reed@reedobrien.com>.
 All rights reserved. Use of this source code is governed by a
 BSD-style license that can be found in the LICENSE file.
@@ -13,6 +13,10 @@ import logging
 import sys
 
 import botocore.session
+from botocore.exceptions import (
+        ApiVersionNotFoundError,
+        ConfigNotFound
+        )
 
 try:
     from yaml import CSafeLoader, CSafeDumper
@@ -141,33 +145,112 @@ example_allow_rw_to_s3 = """{
 }
 """
 
-
 class BTX(object):
+    """
+    BTX is a wrapper for services.
+    Parameters
+
+    ```service```
+    The AWS services we wish to use, e.g. "s3".
+
+    ```debug``` 
+    If the debug flag is set. **Default:** False
+
+    ```logger```
+    Your application's logger should you wish to pass it.
+
+    ```region```
+    The region to connect to. **Default:** us-east-1
+    """
+    def __init__(self, service, debug=False, logger=None, region="us-east-1"):
+        if logger:
+            # TODO: assert it is of type logger
+            self.log = logger
+        elif debug:
+            self.setup_logger(level=logging.DEBUG, service=service)
+        else:
+            self.setup_logger(service=service)
+
+        assert not (debug and logger)
+
+        self._session = botocore.session.get_session()
+        
+        try:
+            self._service = self._session.get_service(service.lower())
+        except ApiVersionNotFoundError as e:
+            self.log.critical(
+                    "Error loading service '{}' ".format(e))
+        ## TODO: does this need try: except?
+        self._ep = self._service.get_endpoint(region)
+        
+
+
+    def __call__(self, operation, **kwargs):
+        self.log.info("Performing {} with {}".format(operation, kwargs))
+        op = self._service.get_operation(operation)
+        r, d = op.call(self._ep, **kwargs)
+        if not r.ok:
+            self.log.error(
+                "Error code: {}, reason: {}".format(
+                    r.status_code, r.reason))
+            self.log.debug("content: {}".format(r.content))
+        # let caller decide if it is an "Exception"
+        return r, d
+
+    
+
+    def setup_logger(self, level=logging.INFO, service="Exception"):
+        log = logging.getLogger("BTX-{}".format(service))
+        log.setLevel(level)
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(level)
+        handler.setFormatter(logging.Formatter(
+            "%(asctime)s %(name)s: [%(levelname)s] %(message)s"))
+        log.addHandler(handler)
+        self.log = log
+
+class Command(BTX):
+    """
+    BTXCmd is a wrapper for botocore which is suitable for command line scripts.
+    
+    ```service```
+    The AWS services we wish to use, e.g. "s3".
+
+    ```debug``` 
+    If the debug flag is set. **Default:** False
+
+    ```logger```
+    Your application's logger should you wish to pass it.
+
+    ```region```
+    The region to connect to. **Default:** us-east-1
+    """
     def __init__(self, config_path,
                  debug=False, dryrun=False, logger=None, region="us-east-1",
                  config_format="yaml"):
-        assert config_format in ("yaml", "json")
+        
+        assert config_format in ("yaml", "json"
+                ), "Unknown configuration format: {}".format(config_format)
         self.config_format = config_format
         self.load_config(config_path)
-        assert not (debug and logger)
-        self.dryrun = dryrun
-        self._session = botocore.session.get_session()
-        self._service = self._session.get_service(
-            self.config["service"].lower())
-        self._ep = self._service.get_endpoint(region)
+        assert not (debug and logger), "Set either debug or logger, not both."
+        ## Setup the logger here with our config service before
+        ## initializing the super class
         if logger:
-            # assert it is type logger
-            self.log = logger
+            self.log= logger
         elif debug:
-            self.setup_logger(level=logging.DEBUG)
+            super(self).setup_logger(
+                level=logging.DEBUG, service=self.config["service"])
         else:
-            self.setup_logger()
-
+            super(self).setup_logger(service=self.config["service"])
+        
+        super(self).__init__(debug=debug, logger=logger, region=region)
+        self.dryrun = dryrun
         if self.dryrun:
             self.log.critical(
                 "dryrun (-n) is set to {}. No action will be taken but "
                 "this is what I would do...".format(self.dryrun))
-
+    
     def __call__(self, operation, **kwargs):
         self.log.info("Performing {} with {}".format(operation, kwargs))
         if not self.dryrun:
@@ -180,6 +263,8 @@ class BTX(object):
                 self.log.debug("content: {}".format(r.content))
             # let caller decide if it is an "Exception"
             return r, d
+
+
 
     def load_config(self, path):
         with open(path) as f:
@@ -198,12 +283,3 @@ class BTX(object):
         ## AWS wants a string of JSON
         return json.dumps(self.load_policy(path))
 
-    def setup_logger(self, level=logging.INFO):
-        log = logging.getLogger("BTX-{}".format(self.config["service"]))
-        log.setLevel(level)
-        handler = logging.StreamHandler(sys.stdout)
-        handler.setLevel(level)
-        handler.setFormatter(logging.Formatter(
-            "%(asctime)s %(name)s: [%(levelname)s] %(message)s"))
-        log.addHandler(handler)
-        self.log = log
